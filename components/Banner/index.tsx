fb585b90ad2ba4edcec29019b24a0a2a8c02164b
Sumit Man <summitmman@gmail.com>
2024-12-25 21:36:28 +0100

    Update banner and introduction content

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -7,7 +7,7 @@ const Banner = () => {
         <section className="mt-6">
             <h1 className={styles.title}>Sumit Man</h1>
             <h2 className={styles.subTitle}>Software Developer</h2>
-            <p>10 years of work exp as an ic and a lead, creates libraries and exp building e-commerce websites</p>
+            <p>I build experiences for the web and bring customizable solutions to problems</p>
             <ul className={styles.social}>
                 <li><a href="https://www.linkedin.com/in/sumitman/" target="_blank"><FaLinkedin /></a></li>
                 <li><a href="https://github.com/summitmman" target="_blank"><FaGithub /></a></li>
